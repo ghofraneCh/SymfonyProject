76ec4fd0a535d45dfac83b70e7746bbc6b067e75
Ghofrane <ghofrane.chakroun@soprahr.com>
2021-05-24 00:11:36 +0100

    fixing upper case and lower case

---
+++ symfony_project/migrations/Version20210521213222.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210521213222 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE commande (id INT AUTO_INCREMENT NOT NULL, facture_id INT DEFAULT NULL, date_commande DATE NOT NULL, UNIQUE INDEX UNIQ_6EEAA67D7F2DEE08 (facture_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE facture (id INT AUTO_INCREMENT NOT NULL, date_facture DATE NOT NULL, prix_total DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE ligne_produit (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE ligne_produit_produit (ligne_produit_id INT NOT NULL, produit_id INT NOT NULL, INDEX IDX_E4BF064DEC9890E6 (ligne_produit_id), INDEX IDX_E4BF064DF347EFB (produit_id), PRIMARY KEY(ligne_produit_id, produit_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE ligne_produit_commande (ligne_produit_id INT NOT NULL, commande_id INT NOT NULL, INDEX IDX_C34034D1EC9890E6 (ligne_produit_id), INDEX IDX_C34034D182EA2E54 (commande_id), PRIMARY KEY(ligne_produit_id, commande_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE livraison (id INT AUTO_INCREMENT NOT NULL, commande_id INT DEFAULT NULL, date_livraison DATE NOT NULL, UNIQUE INDEX UNIQ_A60C9F1F82EA2E54 (commande_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE produit (id INT AUTO_INCREMENT NOT NULL, type_produit_id INT NOT NULL, taille VARCHAR(255) NOT NULL, couleur VARCHAR(255) NOT NULL, nom VARCHAR(255) NOT NULL, quantite INT NOT NULL, prix DOUBLE PRECISION NOT NULL, INDEX IDX_29A5EC271237A8DE (type_produit_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE type_produit (id INT AUTO_INCREMENT NOT NULL, saison VARCHAR(255) NOT NULL, sexe INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE commande ADD CONSTRAINT FK_6EEAA67D7F2DEE08 FOREIGN KEY (facture_id) REFERENCES facture (id)');
+        $this->addSql('ALTER TABLE ligne_produit_produit ADD CONSTRAINT FK_E4BF064DEC9890E6 FOREIGN KEY (ligne_produit_id) REFERENCES ligne_produit (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE ligne_produit_produit ADD CONSTRAINT FK_E4BF064DF347EFB FOREIGN KEY (produit_id) REFERENCES produit (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE ligne_produit_commande ADD CONSTRAINT FK_C34034D1EC9890E6 FOREIGN KEY (ligne_produit_id) REFERENCES ligne_produit (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE ligne_produit_commande ADD CONSTRAINT FK_C34034D182EA2E54 FOREIGN KEY (commande_id) REFERENCES commande (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE livraison ADD CONSTRAINT FK_A60C9F1F82EA2E54 FOREIGN KEY (commande_id) REFERENCES commande (id)');
+        $this->addSql('ALTER TABLE produit ADD CONSTRAINT FK_29A5EC271237A8DE FOREIGN KEY (type_produit_id) REFERENCES type_produit (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE ligne_produit_commande DROP FOREIGN KEY FK_C34034D182EA2E54');
+        $this->addSql('ALTER TABLE livraison DROP FOREIGN KEY FK_A60C9F1F82EA2E54');
+        $this->addSql('ALTER TABLE commande DROP FOREIGN KEY FK_6EEAA67D7F2DEE08');
+        $this->addSql('ALTER TABLE ligne_produit_produit DROP FOREIGN KEY FK_E4BF064DEC9890E6');
+        $this->addSql('ALTER TABLE ligne_produit_commande DROP FOREIGN KEY FK_C34034D1EC9890E6');
+        $this->addSql('ALTER TABLE ligne_produit_produit DROP FOREIGN KEY FK_E4BF064DF347EFB');
+        $this->addSql('ALTER TABLE produit DROP FOREIGN KEY FK_29A5EC271237A8DE');
+        $this->addSql('DROP TABLE commande');
+        $this->addSql('DROP TABLE facture');
+        $this->addSql('DROP TABLE ligne_produit');
+        $this->addSql('DROP TABLE ligne_produit_produit');
+        $this->addSql('DROP TABLE ligne_produit_commande');
+        $this->addSql('DROP TABLE livraison');
+        $this->addSql('DROP TABLE produit');
+        $this->addSql('DROP TABLE type_produit');
+    }
+}
--- symfony_project/src/Controller/WelcomeController.php
+++ symfony_project/src/Controller/WelcomeController.php
@@ -2,16 +2,40 @@
 // src/Controller/WelcomeController.php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Produit;
+use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface;
 
-class WelcomeController extends Controller
+class WelcomeController extends AbstractController
 {
-    public function index(): Response
+    /**
+     * @Route("/produit", name="produit")
+     */
+    public function index(Request $request, PaginatorInterface $paginator)
+    {
+
+        $donnees = $this->getDoctrine()->getRepository(Produit::class)->findBy([],['created_at' => 'desc']);
+
+        $produit = $paginator->paginate(
+            $donnees,
+            $request->query->getInt('page', 1),6
+        );
+
+        return $this->render('welcome/welcome.html.twig', [
+            'produit' => $produit,
+        ]);
+
+    }
+
+
+  /*  public function index(): Response
     {
         return $this->render('welcome/welcome.html.twig');
 
     }
-
+    */
       
 }
