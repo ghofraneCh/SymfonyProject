5421f09024db1a2196aa5d336fb0f80854de1ca9
Ghofrane <ghofrane.chakroun@soprahr.com>
2021-06-03 23:16:21 +0100

    Adding dynamic welcome page with details of the product

---
+++ symfony_project/src/Controller/DetailsProduitController.php
@@ -0,0 +1,35 @@
+<?php
+// src/Controller/WelcomeController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Produit;
+use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface;
+
+class DetailsProduitController extends AbstractController
+{
+    /**
+     * @Route("/produit", name="produit")
+     */
+    public function index($id, Request $request)
+    {
+        $donnees = $this->getDoctrine()->getRepository(Produit::class)->find($id);
+
+        return $this->render('welcome/product-details.html.twig', [
+            'produit' => $donnees,
+        ]);
+
+    }
+
+
+  /*  public function index(): Response
+    {
+        return $this->render('welcome/welcome.html.twig');
+
+    }
+    */
+      
+}
